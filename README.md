faf2f86a2c1bb82053c5aba9ea7c96463e4e61b0
Marley <marley.nodejs@gmail.com>
2019-04-01 02:43:44 +0300

    Обновил скрипты развертывания single kubernetes кластер

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # kubernetes
+
 Kubernetes playground
 
-
 ### Original
+
 https://github.com/justmeandopensource/kubernetes
+
+<br/>
+
+Пароль root: kubeadmin
--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -3,9 +3,9 @@
 # Update hosts file
 echo "[TASK 1] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.42.42.100 kmaster.example.com kmaster
-172.42.42.101 kworker1.example.com kworker1
-172.42.42.102 kworker2.example.com kworker2
+192.168.0.10 master.k8s master
+192.168.0.11 node1.k8s node1
+192.168.0.12 node2.k8s node2
 EOF
 
 # Install docker from Docker-ce repository
