12a4439fd8ec8982ebbbbc5db6c2a7763fc652df
Marley <10372159+webmakaka@users.noreply.github.com>
2020-08-13 19:18:51 +0300

    Update bootstrap.sh

--- v16/bootstrap.sh
+++ v16/bootstrap.sh
@@ -60,7 +60,7 @@ EOF
 
 # Install Kubernetes
 echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
-yum install -y -q kubeadm-1.16.13* kubelet-1.16.13* kubectl-1.16.13* >/dev/null 2>&1
+yum install -y -q kubeadm-1.16.14* kubelet-1.16.14* kubectl-1.16.14* >/dev/null 2>&1
 
 # Start and Enable kubelet service
 echo "[TASK 10] Enable and start kubelet service"
